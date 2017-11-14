684d80a564a45f2839f4382e959983b546c13e91
Fokiruna <alekseyyarchuk@gmail.com>
2017-11-14 11:18:52 +0300

    Add delete method in iis

---
+++ MyWebAPI/MyWebAPI.Api/Controllers/ClientController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutoMapper;
+using MyWebAPI.Api.Models;
+using MyWebAPI.BL.ModelsContract;
+using MyWebAPI.BL.Services.Intefaces;
+
+namespace MyWebAPI.Api.Controllers
+{
+    public class ClientController : Controller
+    {
+        private readonly IClientService _service;
+        public ClientController(IClientService service)
+        {
+            _service = service;
+        }
+        // GET: Client
+        //public ActionResult GetAllClients()
+        //{
+        //    var users =
+        //        Mapper.Map<IEnumerable<ClientContract>, List<Client>>(_service.GetAllClients());
+        //    return View(users);
+        //}
+    }
+}
